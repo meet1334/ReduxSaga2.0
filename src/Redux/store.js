622b1099d9a28af8d388d9af80dfd9af6a043e12
meet.vaghasiya <meetvaghasiya66@gmail.com>
2024-05-02 19:40:31 +0530

    Create saga Middleware

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,14 +1,21 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import createSagaMiddleware from "redux-saga";
+import productSaga from "./product/saga";
 
 // const store = createStore(rootReducer);
-const store = configureStore({ reducer: rootReducer });
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => [sagaMiddleware],
+});
+
+sagaMiddleware.run(productSaga);
 
 export default store;
 
-
-
-//--->  createStore version in redux was deprecated , so now onwards we use redux toolkit 
-//--->  Using redux toolkit we need to change only configureStore in store only 
+//--->  createStore version in redux was deprecated , so now onwards we use redux toolkit
+//--->  Using redux toolkit we need to change only configureStore in store only
 //--->  No need to change in any redux files
